93ffa47f88975f7e31ca6b4f068b7e0803fb2c73
ChandraSekhar <chandrabigdata204@gmail.com>
2025-03-11 20:20:29 +0530

    files has been updated

---
+++ variable.tf
@@ -0,0 +1,36 @@
+variable "aws_access_key" {
+  description = "AWS access key"
+  type        = string
+  default     = "terraform-automation"
+}
+
+variable "aws_secret_key" {
+  description = "AWS secret key"
+  type        = string
+  default     = ""
+}
+
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "ap-south-1"
+}
+
+
+variable "ami" {
+   type        = string
+   description = "Ubuntu AMI ID"
+   default     = "ami-00bb6a80f01f03502"
+}
+
+variable "instance_type" {
+   type        = string
+   description = "Instance type"
+   default     = "t2.micro"
+}
+
+variable "name_tag" {
+   type        = string
+   description = "Name of the EC2 instance"
+   default     = "My EC2 Instance"
+}
