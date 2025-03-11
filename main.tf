140ed513062d730f87469cede3f1c7389dccc44d
ChandraSekhar <chandrabigdata204@gmail.com>
2025-03-11 23:22:19 +0530

    chandra

--- main.tf
+++ main.tf
@@ -1,8 +1,21 @@
-resource "aws_instance" "public_instance" {
- ami           = var.ami
- instance_type = var.instance_type
+# resource "aws_instance" "public_instance" {
+#  ami           = var.ami
+#  instance_type = var.instance_type
 
- tags = {
-   Name = var.name_tag,
- }
+#  tags = {
+#    Name = var.name_tag,
+#  }
+# }
+
+
+provider "aws" {
+    region = "ap-south-1"  
+}
+
+resource "aws_instance" "foo" {
+  ami           = "ami-00bb6a80f01f03502" # us-west-2
+  instance_type = "t2.micro"
+  tags = {
+      Name = "TF-Instance"
+  }
 }
